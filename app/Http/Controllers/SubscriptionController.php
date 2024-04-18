b6a7119a095d3e95f6feb10ae26d028376bf12aa
Sebastiaaan <sebastiaan36@gmail.com>
2024-04-18 12:35:58 +0200

    Update SubscriptionController.php

--- app/Http/Controllers/SubscriptionController.php
+++ app/Http/Controllers/SubscriptionController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class SubscriptionController extends Controller
 {
@@ -16,10 +17,24 @@ class SubscriptionController extends Controller
     public function subscribe($plan)
     {
 
-        $user = auth()->user();
-        $result = $user->newSubscription('main', $plan)->create();
-        return view('subscription.success');
+        $user = Auth::user();
+
+        $name = ucfirst($plan) . ' membership';
+
+        if(!$user->subscribed($name, $plan)) {
+
+            $result = $user->newSubscription($name, $plan)->create();
+
+            if(is_a($result, RedirectToCheckoutResponse::class)) {
+                return $result; // Redirect to Mollie checkout
+            }
+
+            return back()->with('status', 'Welcome to the ' . $plan . ' plan');
+        }
+
+        return back()->with('status', 'You are already on the ' . $plan . ' plan');
     }
+
     public function success()
     {
         return view('subscription.success');
